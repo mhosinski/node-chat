f692661b2dfe6cd5633a25155c6ad826006a9865
Mike Hosinski <mike.hosinski@gmail.com>
2015-03-01 15:07:34 -0500

    Move styles from index.html to external .css file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
