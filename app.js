7d5b5c07873ceca6a2bf1a784523dd9da0ae15dc
Mike Hosinski <mike.hosinski@gmail.com>
2015-03-05 15:05:37 -0500

    Start incorporating redis into app

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis');
+var rclient = redis.createClient();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,6 +15,7 @@ io.on('connection', function(socket) {
 	socket.on('set nickname', function(data) {
 		console.log(data + ' has connected');
 		socket.broadcast.emit('user connected', data);
+		rclient.sadd('users', data);
 	});
 	socket.on('chat message', function(name, msg) {
 		socket.broadcast.emit('chat message', name, msg);	
@@ -23,6 +26,10 @@ io.on('connection', function(socket) {
 	socket.on('cease typing', function(data) {
 		socket.broadcast.emit('cease typing', data);
 	});
+});
+
+rclient.on('connect', function() {
+	console.log('Connected to Redis');
 });
 
 http.listen(3000, function() {
--- public/js/script.js
+++ public/js/script.js
@@ -1,58 +1,58 @@
 (function() {
-	var nickname = prompt('Enter a nickname', 'Guest'),
-		typing = false,		
-		socket = io();
-	
-	$('#submit').on('click', function() {	
-		if ($('#m').val()) {	
-			socket.emit('chat message', nickname, $('#m').val());
-			$('#messages').append(
-				$('<li>').text(nickname + ': ' + $('#m').val())
-			);		
-			$('#m').val('');
-		}
-		return false;		
-	});
-	socket.on('chat message', function(name, msg) {
-		$('#' + name + '-typing').remove();	
-		$('#messages').append($('<li>').text(name + ': ' + msg));		
-	});
-	socket.on('user connected', function(user) {
-		$('#messages').append($('<li>').text(user + ' has joined the room'));	
-		$('#users').append($('<li>').text(user));	
-	});
-	socket.on('user typing', function(data) {					
-		$('#messages').append(
-			$('<li id="' + data + '-typing" class="typing">').text(data+ ' is typing...')
-		);
-	});
-	socket.on('cease typing', function(data) {
-		$('#' + data).remove();	
-	});
-	socket.emit('set nickname', nickname);	
+    var nickname = prompt('Enter a nickname', 'Guest'),
+        typing = false,     
+        socket = io();
+    
+    $('#submit').on('click', function() {   
+        if ($('#m').val()) {    
+            socket.emit('chat message', nickname, $('#m').val());
+            $('#messages').append(
+                $('<li>').text(nickname + ': ' + $('#m').val())
+            );      
+            $('#m').val('');
+        }
+        return false;       
+    });
+    socket.on('chat message', function(name, msg) {
+        $('#' + name + '-typing').remove(); 
+        $('#messages').append($('<li>').text(name + ': ' + msg));       
+    });
+    socket.on('user connected', function(user) {
+        $('#messages').append($('<li>').text(user + ' has joined the room'));   
+        $('#users').append($('<li>').text(user));   
+    });
+    socket.on('user typing', function(data) {                   
+        $('#messages').append(
+            $('<li id="' + data + '-typing" class="typing">').text(data+ ' is typing...')
+        );
+    });
+    socket.on('cease typing', function(data) {
+        $('#' + data).remove(); 
+    });
+    socket.emit('set nickname', nickname);  
 
-	$(document).keypress(function(e) {
-					if (e.which == 13) {
-						e.preventDefault();
-						$('#submit').click();
-						typing = false;
-						$('#m').focus();
-					}	
-				})
-				.ready(function() {
-					$('#messages').append($('<li>').text(nickname + ' has joined the room'));
-					$('#users').append($('<li>').text(nickname));	
-					$('#m').focus()
-						   .on('keyup', function() {
-							   if ($(this).val().length) {
-								   if (typing === false) {	
-									   socket.emit('user typing', nickname);	
-								   }
-								   typing = true;
-							   } else {
-								   typing = false;
-								   socket.emit('cease typing', nickname + '-typing');
-							   }
-						   });
-				});
+    $(document).keypress(function(e) {
+                    if (e.which == 13) {
+                        e.preventDefault();
+                        $('#submit').click();
+                        typing = false;
+                        $('#m').focus();
+                    }   
+                })
+        .ready(function() {
+            $('#messages').append($('<li>').text(nickname + ' has joined the room'));
+            $('#users').append($('<li>').text(nickname));   
+            $('#m').focus()
+                   .on('keyup', function() {
+                       if ($(this).val().length) {
+                           if (typing === false) {  
+                               socket.emit('user typing', nickname);    
+                           }
+                           typing = true;
+                       } else {
+                           typing = false;
+                           socket.emit('cease typing', nickname + '-typing');
+                       }
+                   });
+        });
 })();
